bec8f86915bb1048bb3837a52122bc3c9e554918
oli <oli@qq.com>
2025-11-25 16:32:45 +0800

    Initial Commit

---
+++ SSHTerminal/SSHTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSHTerminalApp.swift
+//  SSHTerminal
+//
+//  Created by huiliu on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SSHTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
